b535573152affb3723fabb3bd0afbfe6924ca248
grig9 <70834200+grig9@users.noreply.github.com>
2022-01-31 15:54:42 +0300

    Create Router.php

---
+++ Router.php
@@ -0,0 +1,24 @@
+<?php
+
+class Route2 {
+  protected $routes;
+
+  public function __construct($routes) {
+    $this->routes = $routes;
+  }
+
+  public function run() {
+    $route = $_SERVER['REQUEST_URI'];
+    var_dump($route);
+    if(array_key_exists($route, $this->routes)) {
+      // var_dump(include( $this->routes[$route] ));
+      echo "Маршрут найден";
+      return true;
+    } else {
+        echo "Маршрут не найден";
+        return false;
+      }
+    }
+
+
+  }
